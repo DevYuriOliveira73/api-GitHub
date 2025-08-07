5504d35be7985ba8863c5e5d796d2a73b26d8c14
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-08-07 18:51:30 -0300

    feat(Search): mobile version style

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,3 +1,25 @@
+import styles from "./Search.module.css";
+import { useRef } from "react";
 export default function Search() {
-  return <h1>Search</h1>;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit() {}
+
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.title}>
+        <span>Search </span>
+        <span>d_evs</span>
+      </h1>
+
+      <form className={styles.input}>
+        <img
+          src="/leading-icon.svg"
+          alt="magnifying glass"
+          className={styles.lupa}
+        />
+        <input ref={inputRef} placeholder="Search" />
+      </form>
+    </section>
+  );
 }
